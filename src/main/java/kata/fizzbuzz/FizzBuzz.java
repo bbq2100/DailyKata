06ebd1bbd6ba9d913d8f1c67ef3a80c5357b3417
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-08 11:15:23 +0100

    FizzBuzz: formatted src

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,14 @@ package kata.fizzbuzz;
 public class FizzBuzz {
 
 	public String getResult(int number) {
+		final int baseOf3 = 3;
+		final int baseOf5 = 5;
+
 		if(isNotAValidNumber(number)) return "Invalid Number";
-		else {
-			final int baseOf3 = 3;
-			final int baseOf5 = 5;
-			if (isMultipleOfBase(number, baseOf3) && isMultipleOfBase(number, baseOf5)) return "FizzBuzz";
-			else if (isMultipleOfBase(number, baseOf3)) return "Fizz";
-			else if (isMultipleOfBase(number, baseOf5)) return "Buzz";
-		}
+		else if (isMultipleOfBase(number, baseOf3) && isMultipleOfBase(number, baseOf5)) return "FizzBuzz";
+		else if (isMultipleOfBase(number, baseOf3)) return "Fizz";
+		else if (isMultipleOfBase(number, baseOf5)) return "Buzz";
+		
 		return getOrdinaryNumber(number);
 	}
 
