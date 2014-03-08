d2ae576e9d3643638927aa3ac58bd6ce0b9dcd6a
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-08 11:59:06 +0100

    RomanNumer: Created Parser class

---
+++ src/main/java/kata/romannumerals/RomanNumeralParser.java
@@ -0,0 +1,5 @@
+package kata.romannumerals;
+
+public class RomanNumeralParser {
+
+}
---
+++ src/test/java/kata/romannumerals/RomanNumeralParserTest.java
@@ -0,0 +1,14 @@
+package kata.romannumerals;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumeralParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
