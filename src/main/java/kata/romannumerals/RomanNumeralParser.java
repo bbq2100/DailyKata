26f7f508f98b649b7c372fa135f761417605126c
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-10 23:36:26 +0100

    RomanFigures as enum

--- src/main/java/kata/romannumerals/RomanNumeralParser.java
+++ src/main/java/kata/romannumerals/RomanNumeralParser.java
@@ -3,7 +3,10 @@ package kata.romannumerals;
 public class RomanNumeralParser {
 
 	public Node parseRomanNumeralToSyntaxTree(String romanNumer) {
-		return new Node();
+		char[] charArray = romanNumer.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			
+		}
 	}
 
 }
