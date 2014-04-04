acd03d1800add8b2a3db5ef0b359c993fd3dc6c3
Qaiser Abbasi <q.abbasi1010@gmail.com>
2014-04-04 12:10:47 +0200

    Specs, traits

--- src/test/java/kata/scala/lang/chapter4/ButtonObserverSpec.scala
+++ src/test/java/kata/scala/lang/chapter4/ButtonObserverSpec.scala
@@ -5,7 +5,7 @@ import org.specs2.runner.JUnitRunner
 import org.specs2.matcher.MustMatchers
 import org.junit.Test
 
-class ButtonObserverSpec extends Specification {
+object ButtonObserverSpec extends Specification {
   "A Button Observer" should {
     "observe button clicks" in {
       val observerableButton = new ObservableButton("Okay")
@@ -13,6 +13,24 @@ class ButtonObserverSpec extends Specification {
       observerableButton.addObserver(buttonObserver)
       
       for(i <- 1 to 3) observerableButton.click()
+      buttonObserver.count mustEqual 3
+    }
+  }
+}
+
+object ButtonObserverTraitMixinSpec extends Specification{
+  "A Button Observer" should {
+    "observer button clicks" in {
+      val observableButton = new Button("Okay") with Subject {  // mixin trait on fly: on object declaration
+        override def click() = {
+          super.click
+          notifyObservers
+        }
+      }
+      val buttonObserver = new ButtonCountObserver
+      observableButton.addObserver(buttonObserver)
+      
+      for(i <- 1 to 3) observableButton.click()
       buttonObserver.count mustEqual 3
     }
   }
